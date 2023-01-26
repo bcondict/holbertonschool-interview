d10b573ae1148c7b321bd2973cf32b2388527a70
Jesus <jesus.junc10@gmail.com>
2023-01-26 15:03:29 -0500

    0 stat new correction

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -3,11 +3,10 @@
 
 
 import sys
-import ipaddress
 
 
 stdinput = sys.stdin
-status_code = [200, 301,  400, 401, 403, 404, 405, 500]
+status_code = ["200", "301", "400", "401", "403", "404", "405", "500"]
 status_dict = {}
 final_size = 0
 
@@ -25,23 +24,18 @@ if __name__ == "__main__":
 
     try:
         for idx, line in enumerate(stdinput):
+            line = line.replace("-", " ", 1)
+            line = line.replace("  ", "")
             data = line.split(" ")
-            ip = data[0]
-            date = data[2].replace("[", "") + " " + data[3].replace("]", "")
-            method_str = data[4] + " " + data[5] + " " + data[6]
-            status = int(data[7])
-            file_size = int(data[8])
-
             try:
-                if not ipaddress.IPv4Address(ip):
-                    print("0")
-                    break
+                ip = data[0]
+                date = data[1].replace("[", "") + " " + data[2].replace("]", "")
+                method_str = data[3] + " " + data[4] + " " + data[5]
+                status = data[6]
+                file_size = int(data[7])
+
                 if method_str != "\"GET /projects/260 HTTP/1.1\"":
-                    print("1")
-                    break
-                if status not in status_code:
-                    print("2")
-                    break
+                    continue
 
                 if status in status_dict:
                     status_dict[status] += 1
@@ -52,8 +46,8 @@ if __name__ == "__main__":
 
                 if idx != 0 and (idx + 1) % 10 == 0:
                     to_print()
-            except Exception:
-                break
+            except Exception as e:
+                pass
         to_print()
     except KeyboardInterrupt:
         to_print()
