62973bef8e3e52de6641def727a3e0a51cf2d3c4
Jesus <jesus.junc10@gmail.com>
2023-04-06 12:27:43 -0500

    style correction

--- 0x0E-linear_skip/0-linear_skip.c
+++ 0x0E-linear_skip/0-linear_skip.c
@@ -10,10 +10,12 @@ skiplist_t *linear_skip(skiplist_t *list, int value)
 
 	while (node->express)
 	{
-		printf("Value checked at index [%lu] = [%d]\n", node->express->index, node->express->n);
+		printf("Value checked at index [%lu] = [%d]\n",
+		node->express->index, node->express->n);
 		if (node->express->n >= value)
 		{
-			printf("Value found between indexes [%lu] and [%lu]\n", node->index, node->express->index);
+			printf("Value found between indexes [%lu] and [%lu]\n",
+			node->index, node->express->index);
 			break;
 		}
 		node = node->express;
@@ -22,9 +24,10 @@ skiplist_t *linear_skip(skiplist_t *list, int value)
 	if (!node->express)
 	{
 		prev = node;
-		while (node->next)
-			node = node->next;
-		printf("Value found between indexes [%lu] and [%lu]\n", prev->index, node->index);
+		/* while (node->next)
+			node = node->next;*/
+		printf("Value found between indexes [%lu] and [%lu]\n",
+			prev->index, node->index);
 	}
 
 	while (node)
