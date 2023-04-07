2c6fb6cf366fecfe3b8e81e8c91ee410ac3f43fd
Jesus <jesus.junc10@gmail.com>
2023-04-06 20:14:32 -0500

    other style error fixed

--- 0x0E-linear_skip/0-linear_skip.c
+++ 0x0E-linear_skip/0-linear_skip.c
@@ -2,10 +2,10 @@
 
 /**
  * linear_skip - searches for a value in a sorted skip list of integers
- * 
- * @list: pointer to the head of the skip list to search in 
- * @value: value to search for 
- * Return: pointer on the first node where value is located, or NULL 
+ *
+ * @list: pointer to the head of the skip list to search in
+ * @value: value to search for
+ * Return: pointer on the first node where value is located, or NULL
  */
 skiplist_t *linear_skip(skiplist_t *list, int value)
 {
