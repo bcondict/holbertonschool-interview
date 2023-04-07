5a00280730735691ab8fd6f0c4e63712126733cb
Jesus <jesus.junc10@gmail.com>
2023-04-06 20:12:58 -0500

    all files returned and error fixed

--- 0x0E-linear_skip/0-linear_skip.c
+++ 0x0E-linear_skip/0-linear_skip.c
@@ -1,5 +1,12 @@
 #include "search.h"
 
+/**
+ * linear_skip - searches for a value in a sorted skip list of integers
+ * 
+ * @list: pointer to the head of the skip list to search in 
+ * @value: value to search for 
+ * Return: pointer on the first node where value is located, or NULL 
+ */
 skiplist_t *linear_skip(skiplist_t *list, int value)
 {
 	skiplist_t *node = list;
@@ -11,11 +18,11 @@ skiplist_t *linear_skip(skiplist_t *list, int value)
 	while (node->express)
 	{
 		printf("Value checked at index [%lu] = [%d]\n",
-		node->express->index, node->express->n);
+			node->express->index, node->express->n);
 		if (node->express->n >= value)
 		{
 			printf("Value found between indexes [%lu] and [%lu]\n",
-			node->index, node->express->index);
+				node->index, node->express->index);
 			break;
 		}
 		node = node->express;
