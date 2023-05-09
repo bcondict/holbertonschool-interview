179459713afd0a939726fad502fd448196eaa878
Jesus <jesus.junc10@gmail.com>
2023-05-09 12:23:27 -0500

    complete task done

---
+++ 0x13-count_it/0-count.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""
+Query Reddit API, parse titles of all hot articles, and print a sorted count of
+given keywords (case-insensitive, delimited by spaces
+"""
+
+import requests
+
+
+def count_words(subreddit, word_list, counts={}, after=None):
+    """
+        method that queries the Reddit API, parses the title of all hot
+        articles and prints a sorted count of given keywords
+        (case-insensitive, delimited by spaces.
+    """
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+
+    headers = {'User-agent': 'holberton_school'}
+    params = {'limit': 100, 'after': after}
+
+    response = requests.get(
+        url,
+        headers=headers,
+        params=params,
+        allow_redirects=False
+    )
+
+    if response.status_code != 200:
+        return
+
+    data = response.json().get('data').get('children')
+    after = response.json().get('data').get('after')
+
+    for child in data:
+        title = child.get('data').get('title').lower()
+
+        for word in word_list:
+            ocurrences = title.count(word.lower())
+            if ocurrences > 0:
+                if word.lower() not in counts:
+                    counts[word.lower()] = ocurrences
+                else:
+                    counts[word.lower()] += ocurrences
+
+        count_words(subreddit, word_list, counts, after)
+
+    else:
+        if not counts:
+            return
+
+        sorted_counts = sorted(counts.items(), key=lambda x: (-x[1], x[0]))
+        for word, count in sorted_counts:
+            print(f"{word} : {count}")
