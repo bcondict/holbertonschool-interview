d188b85130d5296fe374e6294537c5a16b65ea90
Jesus <jesus.junc10@gmail.com>
2023-05-22 20:11:27 -0500

    header file

---
+++ 0x15-infinite_multiplication/holberton.h
@@ -0,0 +1,16 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+/* Libraries */
+#include <stdlib.h>
+
+/* Prototypes */
+int _putchar(char c);
+char *multiply(char *num1, char *num2);
+int str_len(char *str);
+int is_digit(char c);
+void print_error(void);
+void print_result(char *result);
+
+
+#endif /* HOLBERTON_H */
