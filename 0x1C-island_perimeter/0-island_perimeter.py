6e71d8e3a5af9521c28d99bbb421125bf4974004
Jesús <98342185+bcondict@users.noreply.github.com>
2023-07-28 03:48:25 +0000

    comments correction

--- 0x1C-island_perimeter/0-island_perimeter.py
+++ 0x1C-island_perimeter/0-island_perimeter.py
@@ -14,22 +14,22 @@ def island_perimeter(grid):
         Return: perimeter of the island described in grid
     """
     perimeter = 0
-    for row_idx, row in enumerate(grid):
     # go through each row in the grid
-        for col_idx, col in enumerate(row):
+    for row_idx, row in enumerate(grid):
         # go through each column in the row
-            if col == 1:
+        for col_idx, col in enumerate(row):
             # if the current cell is land, add 4 to the perimeter
+            if col == 1:
                 perimeter += 4
-                if row[col_idx - 1] > 0:
                 # if the cell to the left is land, subtract 2 from the
                 # perimeter because the left cell's right side is touching
                 # the current cell's left side
+                if row[col_idx - 1] > 0:
                     perimeter -= 2
-                if grid[row_idx - 1][col_idx] > 0:
                 # if the cell above is land, subtract 2 from the perimeter
                 # because the above cell's bottom side is touching
                 # the current cell's top side
+                if grid[row_idx - 1][col_idx] > 0:
                     perimeter -= 2
 
     return perimeter
