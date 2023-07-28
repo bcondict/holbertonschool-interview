9536573453357eb5e72756997e315d1a71e03107
Jesús <98342185+bcondict@users.noreply.github.com>
2023-07-28 03:45:10 +0000

    changes documentation and if statement ==  1 -> > 0

--- 0x1C-island_perimeter/0-island_perimeter.py
+++ 0x1C-island_perimeter/0-island_perimeter.py
@@ -5,24 +5,31 @@
 
 
 def island_perimeter(grid):
-    """Returns the perimeter of the island described in grid"""
+    """
+        Calulcate the perimeter of the island described in grid
+
+        Args:
+            grid ([[int]]): is a list of list of integers
+
+        Return: perimeter of the island described in grid
+    """
     perimeter = 0
     for row_idx, row in enumerate(grid):
-        # go through each row in the grid
+    # go through each row in the grid
         for col_idx, col in enumerate(row):
-            # go through each column in the row
+        # go through each column in the row
             if col == 1:
-                # if the current cell is land, add 4 to the perimeter
+            # if the current cell is land, add 4 to the perimeter
                 perimeter += 4
-                if row[col_idx - 1] == 1:
-                    # if the cell to the left is land, subtract 2 from the
-                    # perimeter because the left cell's right side is touching
-                    # the current cell's left side
+                if row[col_idx - 1] > 0:
+                # if the cell to the left is land, subtract 2 from the
+                # perimeter because the left cell's right side is touching
+                # the current cell's left side
                     perimeter -= 2
-                if grid[row_idx - 1][col_idx] == 1:
-                    # if the cell above is land, subtract 2 from the perimeter
-                    # because the above cell's bottom side is touching
-                    # the current cell's top side
+                if grid[row_idx - 1][col_idx] > 0:
+                # if the cell above is land, subtract 2 from the perimeter
+                # because the above cell's bottom side is touching
+                # the current cell's top side
                     perimeter -= 2
 
     return perimeter
