88a0538e99701ca149b75dd975346f577767f183
Jesus <jesus.junc10@gmail.com>
2023-10-13 01:02:56 -0500

    radix_sort

---
+++ radix_sort/sort.h
@@ -0,0 +1,20 @@
+#ifndef _SORT_H_
+#define _SORT_H_
+
+/* Libraries*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+/* Prototypes*/
+void print_array(const int *array, size_t size);
+
+void radix_sort(int *array, size_t size);
+/*
+ * void significant_digit(int *array, size_t size, int digit);
+ * void swap(int *xp, int *yp);
+ * int length_greatest_number(int *array, size_t size);
+ * double custom_pow(double base, int exponent);
+*/
+
+#endif
