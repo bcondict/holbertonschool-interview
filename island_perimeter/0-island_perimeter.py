c9a6802087d62f9750f49a635b6f4200f51b7d5b
Jesus <jesus.junc10@gmail.com>
2023-10-20 01:44:50 -0500

    fixed issues

--- island_perimeter/0-island_perimeter.py
+++ island_perimeter/0-island_perimeter.py
@@ -24,12 +24,12 @@ def island_perimeter(grid):
                 # if the cell to the left is land, subtract 2 from the
                 # perimeter because the left cell's right side is touching
                 # the current cell's left side
-                if row[col_idx - 1] > 0:
+                if row[col_idx - 1] > 0 and col_idx != 0:
                     perimeter -= 2
                 # if the cell above is land, subtract 2 from the perimeter
                 # because the above cell's bottom side is touching
                 # the current cell's top side
-                if grid[row_idx - 1][col_idx] > 0:
+                if grid[row_idx - 1][col_idx] > 0 and row_idx != 0:
                     perimeter -= 2
 
     return perimeter
