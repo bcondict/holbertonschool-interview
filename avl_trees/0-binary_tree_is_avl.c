b2cc71a9d353ca28c34d4cb7e3119e74d1790e01
Jesus <jesus.junc10@gmail.com>
2023-10-24 17:35:08 -0500

    added condition if left and right 0

--- avl_trees/0-binary_tree_is_avl.c
+++ avl_trees/0-binary_tree_is_avl.c
@@ -19,6 +19,8 @@ int binary_tree_is_avl(const binary_tree_t *tree)
 	left = binary_tree_height(tree->left);
 	right = binary_tree_height(tree->right);
 
+	if (left == 0 && right == 0)
+		return (1);
 	if (abs(left - right) > 1)
 		return (0);
 
